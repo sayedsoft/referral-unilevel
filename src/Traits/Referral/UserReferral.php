af6b546fa713b1560fd9f85eab2b53758641c20c
yemowith <yemowith@gmail.com>
2025-03-27 22:41:45 +0300

    update

--- src/Traits/Referral/UserReferral.php
+++ src/Traits/Referral/UserReferral.php
@@ -8,43 +8,43 @@ use Sayedsoft\ReferralUnilevel\Models\Referral\Referral;
 use Sayedsoft\ReferralUnilevel\Models\Referral\ReferralSponsor;
 
 trait UserReferral
-{   
+{
 
     public function initializeUserReferralTrait()
     {
         $this->appends[] = 'referral_code';
     }
 
-    public function referral() {
-        return $this->belongsTo(\Sayedsoft\ReferralUnilevel\Models\Referral\Referral::class,'id','user_id');
+    public function referral()
+    {
+        return $this->belongsTo(\Sayedsoft\ReferralUnilevel\Models\Referral\Referral::class, 'id', 'user_id');
     }
 
     /*
       @referral code 
     */
-    public function initReferral($referral_code) {
+    public function initReferral($referral_code)
+    {
 
         if (!empty($this->referral)) {
-            throw(new Exception('User has Referral already'));
+            throw (new Exception('User has Referral already'));
         }
 
         Referral::create([
-            'user_id'       => $this->id,
-            'referral_id'   => Referral::where('referral_code',$referral_code)->first()->user_id,
+            'user_id' => $this->id,
+            'referral_id' => Referral::where('referral_code', $referral_code)->first()->user_id,
             'referral_code' => ReferralCode::generateCode(),
         ]);
-        
+
     }
 
 
-    
+
     public static function boot()
     {
         parent::boot();
-
-
     }
 
-    
+
 
 }
